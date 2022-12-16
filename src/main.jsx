f499b04ed7e0a7ca5ad6ed4f073d884da4334ab4
Ana Lucia Castellanos <119083326+analucv@users.noreply.github.com>
2022-12-15 20:22:07 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CounterApp } from './CounterApp';
 import './styles.css';
-// import { HelloWorldApp } from './HelloWorldApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
